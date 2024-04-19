a6bac9bf4dc63d197ab683b73d11697252b8ecf3
Heiko van Elsuwe <heiko.vanelsuwe@ista.com>
2024-04-19 10:25:26 +0200

    corrects euclidian

--- scripts/module.js
+++ scripts/module.js
@@ -7,7 +7,7 @@ Hooks
 import { registerManhattanRuler } from "./patching.js";
 import { registerSettings, getSetting, SETTINGS } from "./settings.js";
 
-export const MODULE_ID = "euclidianruler";
+export const MODULE_ID = "euclideanruler";
 
 export function log(...args) {
   try {
